d9a3b7f11298562748024e4af522003b10413245
cisco <ciscoducasse99@hotmail.com>
2019-08-06 14:43:11 -0400

    setting config variables

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Importing packages needed to run servers and DB
 const mongoose = require('mongoose')
 const express = require('express')
-require('dotenv').config()
 const port = 5000
 const path = require('path')
 
@@ -13,9 +12,10 @@ app.use(express.json())
 
 //Connecting to MongoDB using Mongoose
 mongoose
-    .connect(process.env.MONGO_URI, {useNewUrlParser: true})
+    .connect(process.env.MONGO_KEY, {useNewUrlParser: true})
     .then( () =>{
         console.log('MongoDB connected via Mongoose')
+        console.l0g('mongo key is:' + process.env.MONGO_KEY)
     })
     .catch( err => {
         console.log('error message: ' + err)
